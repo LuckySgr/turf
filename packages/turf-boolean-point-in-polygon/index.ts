95c6f45fcc3191b703fa93c3db9b6bf2cb602e99
xiexl <742875110@qq.com>
2024-04-08 15:40:10 +0800

    梳理判断点是否在多边形上的代码逻辑

--- packages/turf-boolean-point-in-polygon/index.ts
+++ packages/turf-boolean-point-in-polygon/index.ts
@@ -13,6 +13,7 @@ import { getCoord, getGeom } from "@turf/invariant";
 // modified from: https://github.com/substack/point-in-polygon/blob/master/index.js
 // which was modified from http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
 /**
+ * 判断点是否在多边形内
  * Takes a {@link Point} and a {@link Polygon} or {@link MultiPolygon} and determines if the point
  * resides inside the polygon. The polygon can be convex or concave. The function accounts for holes.
  *
@@ -21,7 +22,7 @@ import { getCoord, getGeom } from "@turf/invariant";
  * @param {Feature<Polygon|MultiPolygon>} polygon input polygon or multipolygon
  * @param {Object} [options={}] Optional parameters
  * @param {boolean} [options.ignoreBoundary=false] True if polygon boundary should be ignored when determining if
- * the point is inside the polygon otherwise false.
+ * the point is inside the polygon otherwise false. 是否需要忽略多边形边界
  * @returns {boolean} `true` if the Point is inside the Polygon; `false` if the Point is not inside the Polygon
  * @example
  * var pt = turf.point([-77, 44]);
@@ -54,14 +55,14 @@ function booleanPointInPolygon<
     throw new Error("polygon is required");
   }
 
-  const pt = getCoord(point);
-  const geom = getGeom(polygon);
-  const type = geom.type;
-  const bbox = polygon.bbox;
-  let polys: any[] = geom.coordinates;
+  const pt = getCoord(point); // 点坐标
+  const geom = getGeom(polygon); // 从 GeoJSON 中获取 Geometry
+  const type = geom.type; // 从 Geometry 中获取几何体类型
+  const bbox = polygon.bbox; // 获取面的包围盒
+  let polys: any[] = geom.coordinates; // 从 Geometry 中获取坐标
 
-  // Quick elimination if point is not inside bbox
-  if (bbox && inBBox(pt, bbox) === false) {
+  // 如果点不在 bbox 内，则快速判断其不在多边形内
+  if (bbox && !inBBox(pt, bbox)) {
     return false;
   }
 
@@ -71,7 +72,7 @@ function booleanPointInPolygon<
   let result = false;
   for (var i = 0; i < polys.length; ++i) {
     const polyResult = pip(pt, polys[i]);
-    if (polyResult === 0) return options.ignoreBoundary ? false : true;
+    if (polyResult === 0) return !options.ignoreBoundary;
     else if (polyResult) result = true;
   }
 
@@ -79,6 +80,7 @@ function booleanPointInPolygon<
 }
 
 /**
+ * 判断点是不是在包围盒内部
  * inBBox
  *
  * @private
